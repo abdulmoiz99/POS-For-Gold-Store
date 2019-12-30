104d9ac1214a2ed75ddfc8ce4b0a12aaa5271f09
moiz <moiza0799@gmail.com>
2019-12-30 16:07:58 +0500

    Login Screen

---
+++ POS_GoldStore/UserManagemen/frm_Login.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace POS_GoldStore.UserManagemen
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Login));
+            this.button2 = new System.Windows.Forms.Button();
+            this.lab_CreateAccount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmb_CompanyName = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(254, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 27);
+            this.button2.TabIndex = 49;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lab_CreateAccount
+            // 
+            this.lab_CreateAccount.AutoSize = true;
+            this.lab_CreateAccount.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_CreateAccount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            this.lab_CreateAccount.Location = new System.Drawing.Point(18, 523);
+            this.lab_CreateAccount.Name = "lab_CreateAccount";
+            this.lab_CreateAccount.Size = new System.Drawing.Size(97, 17);
+            this.lab_CreateAccount.TabIndex = 48;
+            this.lab_CreateAccount.Text = "Create Acount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(17, 456);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 21);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Company";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Location = new System.Drawing.Point(16, 441);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(243, 2);
+            this.panel2.TabIndex = 46;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(18, 349);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(241, 2);
+            this.panel1.TabIndex = 45;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn_Login.FlatAppearance.BorderSize = 0;
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.btn_Login.Location = new System.Drawing.Point(18, 555);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(244, 41);
+            this.btn_Login.TabIndex = 44;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_Password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_Password.Location = new System.Drawing.Point(18, 411);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(225, 13);
+            this.txt_Password.TabIndex = 43;
+            this.txt_Password.Enter += new System.EventHandler(this.txt_Password_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(14, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 21);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Password";
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_Username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Username.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_Username.Location = new System.Drawing.Point(21, 319);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(228, 20);
+            this.txt_Username.TabIndex = 41;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(14, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 21);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "UserName";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.label8.Location = new System.Drawing.Point(54, 213);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 28);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "Mobile Store";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(3, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 185);
+            this.button1.TabIndex = 38;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cmb_CompanyName
+            // 
+            this.cmb_CompanyName.FormattingEnabled = true;
+            this.cmb_CompanyName.Location = new System.Drawing.Point(18, 489);
+            this.cmb_CompanyName.Name = "cmb_CompanyName";
+            this.cmb_CompanyName.Size = new System.Drawing.Size(241, 21);
+            this.cmb_CompanyName.TabIndex = 50;
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 618);
+            this.Controls.Add(this.cmb_CompanyName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lab_CreateAccount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Name = "frm_Login";
+            this.Text = "frm_Login";
+            this.Load += new System.EventHandler(this.frm_Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lab_CreateAccount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmb_CompanyName;
+    }
+}
---
+++ POS_GoldStore/UserManagemen/frm_Login.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_GoldStore.UserManagemen
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+        private bool Check()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SQL.DataSource))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connrction to the Sql Server Failed Application is Exiting", "Exit", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+
+            if (cmb_CompanyName.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please Select Company", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Main.CompanyID = int.Parse(cmb_CompanyName.SelectedValue.ToString());
+                if (txt_Username.Text != "")
+                {
+
+                    if (txt_Password.Text != "")
+                    {
+                        if (Main.CheckUserName(txt_Username.Text) == true)
+                        {
+                            if (Main.checkPassword(txt_Username.Text, txt_Password.Text) == true)
+                            {
+                                Main.UserName = txt_Username.Text;
+
+                                this.Hide();
+                                //var NEW = new frm_Main();
+                                // NEW.ShowDialog();
+
+                            }
+                            else MessageBox.Show("Invalid Password", "Login", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                        }
+                        else MessageBox.Show("Invalid Username", "Login", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                    }
+                    else MessageBox.Show("Please Enter Password", "Login", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                else MessageBox.Show("Please Enter UserName", "Login", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+            if (Check() == false)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                String Company = SQL.ScalarQuery("SELECT COUNT(CompanyID) FROM CompanySetup where CompanyActive=1");
+                if (Company == "0")
+                {
+                    MessageBox.Show("No Company Is Created " + System.Environment.NewLine + "Please Create A company First ", "Suggestion", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    try
+                    {
+                        this.Hide();
+                        // var frm = new frm_AddRole();
+                        //frm.ShowDialog();
+                        this.Close();
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                else
+                {
+                    Main.getCompany(cmb_CompanyName, "CompanySetup", "CompanyName", "CompanyId", "CompanyActive=1");
+                    txt_Username.Focus();
+                }
+            }
+        }
+
+        private void txt_Password_Enter(object sender, EventArgs e)
+        {
+            txt_Password.PasswordChar = '●';
+        }
+    }
+}
